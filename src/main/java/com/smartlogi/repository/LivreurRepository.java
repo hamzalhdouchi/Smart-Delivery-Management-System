cadab9ed735bf8d3cab24d03bd2c8e5521c13177
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-22 14:23:21 +0100

    feat : add LivreurRepository interface for Livreur data access
    https://hamzaalhadouchi-1735.atlassian.net/browse/SDMS-2

---
+++ src/main/java/com/smartlogi/repository/LivreurRepository.java
@@ -0,0 +1,7 @@
+package com.smartlogi.repository;
+
+import com.smartlogi.entity.Livreur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivreurRepository extends JpaRepository<Livreur, Long> {
+}
