1e0c0821e9c7769c9a50fc335a4a608fd32264e3
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-22 14:07:30 +0100

    feat : add Livreur entity with attributes and relationships
    https://hamzaalhadouchi-1735.atlassian.net/browse/SDMS-2

---
+++ src/main/java/com/smartlogi/entity/Livreur.java
@@ -0,0 +1,32 @@
+package com.smartlogi.entity;
+
+import com.smartlogi.entity.Colis;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.processing.Pattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Livreur {
+    @Id
+    private Long id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String vehicule;
+
+
+    private String telephone;
+
+
+    private String email;
+
+    @OneToMany(mappedBy = "livreur", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Colis> colisList = new ArrayList<>();
+}
