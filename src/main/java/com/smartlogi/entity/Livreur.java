41af67dff94cc47d9da9db8e91505abc1b07f724
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-22 14:12:38 +0100

    feat : add constructors and update annotations in Livreur entity
    https://hamzaalhadouchi-1735.atlassian.net/browse/SDMS-2

--- src/main/java/com/smartlogi/entity/Livreur.java
+++ src/main/java/com/smartlogi/entity/Livreur.java
@@ -3,14 +3,19 @@ package com.smartlogi.entity;
 import com.smartlogi.entity.Colis;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.processing.Pattern;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Table(name = "livreur")
 public class Livreur {
     @Id
     private Long id;
@@ -29,4 +34,5 @@ public class Livreur {
 
     @OneToMany(mappedBy = "livreur", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Colis> colisList = new ArrayList<>();
+
 }
