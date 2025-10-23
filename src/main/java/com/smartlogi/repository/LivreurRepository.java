10390f87cd08ec33ef3409b9cee7c1a8ea62a8b8
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-23 16:13:12 +0100

    feat : add method to LivreurRepository for retrieving ID

--- src/main/java/com/smartlogi/repository/LivreurRepository.java
+++ src/main/java/com/smartlogi/repository/LivreurRepository.java
@@ -4,4 +4,5 @@ import com.smartlogi.entity.Livreur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivreurRepository extends JpaRepository<Livreur, Long> {
+    Long id(Long id);
 }
