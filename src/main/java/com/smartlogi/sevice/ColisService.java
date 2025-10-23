22f07b69d3618b9aad779d5e10526b0b645fe2d0
hamzalhadouchi <hamzabniss16@gmail.com>
2025-10-23 16:11:26 +0100

    feat : update ColisService to accept CreateColisRequest DTO in saveColis method

--- src/main/java/com/smartlogi/sevice/ColisService.java
+++ src/main/java/com/smartlogi/sevice/ColisService.java
@@ -1,5 +1,6 @@
 package com.smartlogi.sevice;
 
+import com.smartlogi.dto.CreateColisRequest;
 import com.smartlogi.entity.Colis;
 import com.smartlogi.enums.ColisStatus;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ColisService {
-    Colis saveColis(Colis colis);
+    Colis saveColis(CreateColisRequest colisDTO);
     Optional<Colis> getColisById(Long id);
     List<Colis> getAllColis();
     List<Colis> getColisByLivreurId(Long livreurId);
